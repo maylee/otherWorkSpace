8912ffb506ecbcc9a82f07078515ab1f5c91648f
cio <cio@mitlab.kr>
2019-04-28 21:13:25 +0900

    라우터 구현 테스트

--- server.js
+++ server.js
@@ -3,17 +3,12 @@ var app = express();
 const port = 3000
 
 // 시작 메시지
-console.log('Server start!!!');
+console.log('Server start!!!!');
 
 // app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log("Example app listening on port"));
 
-
-// GET method route
-app.get('/', function (req, res) {
-    res.send('GET request to the homepage');
-});
-   
+ 
 // POST method route
 app.post('/', function (req, res) {
     res.send('POST request to the homepage');
