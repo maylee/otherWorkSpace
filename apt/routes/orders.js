07f1ade236496070fcdaba8a46024de20d73a889
cio <cio@mitlab.kr>
2019-04-29 08:42:50 +0900

    라우터들 요구처리에 대해서 공부해보았다.

--- apt/routes/orders.js
+++ apt/routes/orders.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 router.get('/', (req, res) => {
     res.status(200).json({
-        message : "Get /Orders"
+        message : "Get /Orders111"
     });
+    console.log('Router On - Orders');
 });
+
+
+// get으로 받은 주문번호를 판단
+router.get('/:orderId', (req, res) => {
+    
+    const id = req.params.orderId;
+    if( id !== '1234' ){
+        res.status(200).json({
+            message : "worng"
+        });    
+    } else {
+        res.status(200).json({
+            message : "right id",
+            id : id
+        });
+
+    };
+});
+
+
 
 module.exports = router;
 
--- apt/routes/products.js
+++ apt/routes/products.js
@@ -7,4 +7,35 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:productId', (req, res) => 
+{
+    const id = req.params.productId;
+    if( id === 'special' ) {
+        res.status(200).json ({
+            message: '스페셜 아이디',
+            id : id 
+        });
+    } else {
+        res.status(200).json({
+            message: '그냥 아이디'
+        });
+    }
+});
+
+
+router.post('/', (req, res) => {
+    const product = {
+        name : req.body.name,
+        price : req.body.price
+    };
+    res.status(200).json({
+        message : "POST Products",
+        createProducts : product
+    });
+});
+
+
+
+
+
 module.exports = router;
